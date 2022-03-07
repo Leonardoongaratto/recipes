0551621744b17008aa0edf5d7a6bd8fd99fafaa4
Leonardo Custodio Ongaratto <leonardoongaratto@mpto.mp.br>
2022-03-07 14:22:11 -0300

    Removido contrato e sobre.

--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html', context={'name': 'Leonardo'})
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
