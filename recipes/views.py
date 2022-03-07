0527eb0702aefb3647721423851e0cbfb56dce2f
Leonardo Custodio Ongaratto <leonardoongaratto@mpto.mp.br>
2022-03-07 15:43:32 -0300

    Foi ajustado o estilo CSS da Header

--- recipes/views.py
+++ recipes/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Leonardo'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'Leonardo'})
