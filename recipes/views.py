c3bbcf5a599554429bdacb17d52422ea43fa48af
Leonardo Custodio Ongaratto <leonardoongaratto@mpto.mp.br>
2022-03-16 16:22:06 -0300

    alterando a forma de mostrar o erro 404 para paginas não utilizadas, utilizando shortcut get_list_ot_404.

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from unicodedata import category
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from django.http import Http404, HttpResponse
 from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
@@ -13,11 +13,11 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
 
-    if not recipes:
-        raise Http404('Not Found')
-
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id'))
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes.first().category.name} - Category |'
