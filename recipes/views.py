795ce87a964233fbe0337a4ed97e6f1c209f01b9
Leonardo Custodio Ongaratto <leonardoongaratto@mpto.mp.br>
2022-03-17 14:29:39 -0300

    Alterações

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,11 @@
-from unicodedata import category
-from django.shortcuts import get_list_or_404, render
-from django.http import Http404, HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -18,6 +17,7 @@ def category(request, category_id):
         category__id=category_id,
         is_published=True
     ).order_by('-id'))
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category |'
@@ -25,11 +25,8 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        id=id,
-        is_published=True
-    ).order_by('-id').first()
-
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+    
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
