b94d8af3f5e32bf1df28f1ca2539fd7dc38edc6c
Leonardo Custodio Ongaratto <leonardoongaratto@mpto.mp.br>
2022-03-23 16:45:01 -0300

    Foi testado as urls. Foi testado as views utilizando o self.client() do django pra simular os clientes nos testes

---
+++ recipes/tests/test_recipe_urls.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+
+class RecipeURLsTest(TestCase):
+    def test_recipe_url_home_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
+
+
+    def test_recipe_category_url_home_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 2})
+        self.assertEqual(url, '/recipes/category/2/')
+
+
+    def test_recipe_detail_url_home_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 2})
+        self.assertEqual(url, '/recipes/2/')
