58f491309ec572d39b31bf89b1bfd436db3ecb0c
Leonardoongaratto <leonardocongarattodev@gmail.com>
2022-03-30 17:49:50 -0300

    Foi adicionado mais testes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe, User
+
 
 
 class RecipeViewsTest(TestCase):
@@ -27,7 +29,39 @@ class RecipeViewsTest(TestCase):
         )
 
 
-    def test_recipe_home_template_loads_recipe(self):
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Categoria')
+        author = User.objects.create_user(
+            first_name = 'user',
+            last_name = 'name',
+            username = 'username',
+            password = '123456',
+            email = 'user@gmail.com'
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
+
         ...
 
 
