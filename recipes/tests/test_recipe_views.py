e298478d79fcfdaf72156b6a0066a1582d2b84c3
Leonardoongaratto <leonardocongarattodev@gmail.com>
2022-04-01 15:08:29 -0300

    Testando o caminho is_published  True e False

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -42,6 +42,18 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertEqual(len(response_context_recipes), 1)
 
 
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        """Teste recipe is_published False dont show"""
+        # Need a recipe for this test
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        self.assertIn(
+            '<h1> No recipes found here</h1>',
+            response.content.decode('utf-8')
+        )
+
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:category', kwargs={'category_id': 1000})
