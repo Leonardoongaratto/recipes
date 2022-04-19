23f50a28135b019e9bff3a8fced8d07e871c4284
Leonardoongaratto <leonardocongarattodev@gmail.com>
2022-04-19 15:07:05 -0300

    Utilizando Q para mesclar AND e OR com consultas complexas e filtros já existentes.

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from recipes.models import Recipe
+from django.db.models import Q
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 
@@ -39,7 +40,17 @@ def search(request):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) | 
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
     })
