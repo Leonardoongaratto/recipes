253e88f6a16f6c42e28295cfbcb12ae1e045d0b8
Leonardoongaratto <leonardocongarattodev@gmail.com>
2022-04-26 15:28:10 -0300

    Mais exemplos de variáveis de ambiente com python-dotenv - SECRET_KEY e DEBUG

--- app/settings.py
+++ app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*v-hh@08+sl1+$=vvlk2tx@v&93@qtusq=1pf391m!sd8qvfo$'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
+#'django-insecure-*v-hh@08+sl1+$=vvlk2tx@v&93@qtusq=1pf391m!sd8qvfo$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = []
 
