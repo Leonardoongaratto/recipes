baae46a826611daa5ad62edaf2101224496c715d
Leonardoongaratto <leonardocongarattodev@gmail.com>
2022-04-26 14:51:55 -0300

    python-dotenv - movendo PER_PAGE para variáveis de ambiente no Django (.env)

--- recipes/views.py
+++ recipes/views.py
@@ -4,13 +4,17 @@ from django.db.models import Q
 from utils.pagination import make_pagination
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
+import os
+
+PER_PAGE = os.environ.get('PER_PAGE', 4)
+
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 4)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -25,7 +29,7 @@ def category(request, category_id):
         is_published=True
     ).order_by('-id'))
 
-    page_obj, pagination_range = make_pagination(request, recipes, 4)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
 
     return render(request, 'recipes/pages/category.html', context={
@@ -58,7 +62,7 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 4)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
