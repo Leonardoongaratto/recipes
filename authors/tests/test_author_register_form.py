a7e67a77dc95a0a9533043add4cd01785f9f9a9b
Leonardoongaratto <leonardocongarattodev@gmail.com>
2022-05-13 17:29:29 -0300

    Diferença entre teste unitário e teste de integração

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,6 +1,8 @@
 from authors.forms import RegisterForm
 from django.test import TestCase
 from parameterized import parameterized
+from django.test import TestCase as DjangoTestCase
+from django.urls import reverse
 
 
 class AuthorRegisterFormUnitTest(TestCase):
@@ -52,3 +54,24 @@ class AuthorRegisterFormUnitTest(TestCase):
         current = form[field].field.label
         self.assertEqual(current, needed)
     
+
+class AuthorRegisterFormIntegrationTest(DjangoTestCase):
+    def setUp(self, *args, **kwargs):
+        self.form_data = {
+            'username': 'user',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'email@anyemail.com',
+            'password': 'Str0ngP@ssword1',
+            'password2': 'Str0ngP@ssword1',
+        }
+        return super().setUp(*args, **kwargs)
+
+    @parameterized.expand([
+        ('username', 'This field must not be empty'),
+    ])
+    def test_fields_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
